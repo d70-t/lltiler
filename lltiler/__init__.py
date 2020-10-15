9f928d1b5c1eced84be1e1a1cac1cbef2f79cc0c
Tobias Kölling <tobias.koelling@physik.uni-muenchen.de>
2020-10-15 17:36:33 +0200

    export render_tile function

--- lltiler/__init__.py
+++ lltiler/__init__.py
@@ -1,3 +1,3 @@
-from .lltiler import LLTiler, AllTileSelector, ChunkTileSelector
+from .lltiler import LLTiler, AllTileSelector, ChunkTileSelector, render_tile
 
-__all__ = ['LLTiler', 'AllTileSelector', 'ChunkTileSelector']
+__all__ = ['LLTiler', 'AllTileSelector', 'ChunkTileSelector', 'render_tile']
